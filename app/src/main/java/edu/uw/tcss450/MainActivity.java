502c6a648f17a6f1bf8cd261c1d5c4ec3d1652d5
Alex CO <alejandrocossio1@gmail.com>
2021-04-27 20:26:30 -0700

    Added a Weather fragment with some temporary text and added navigation to the weather page through the addition of a weather tab on the bottom navigation menu.

--- app/src/main/java/edu/uw/tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.success, R.id.navigation_blogs)
+                R.id.success, R.id.navigation_blogs, R.id.weather_display)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
