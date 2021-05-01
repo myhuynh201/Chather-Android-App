484998ce9f7b26586a1a3706f89f14633b17dfc9
DewWee05 <duynguyen1999@hotmail.com>
2021-05-01 13:26:14 -0700

    Add some changes into the sign in and register to make it cleaner.

--- app/src/main/java/edu/uw/tcss450/ui/blog/BlogListFragment.java
+++ app/src/main/java/edu/uw/tcss450/ui/blog/BlogListFragment.java
@@ -6,9 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +14,6 @@ import android.view.ViewGroup;
 
 import edu.uw.tcss450.R;
 import edu.uw.tcss450.databinding.FragmentBlogListBinding;
-import edu.uw.tcss450.ui.model.UserInfoViewModel;
 
 
 public class BlogListFragment extends Fragment {
@@ -34,22 +31,6 @@ public class BlogListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.fragment_blog_list, container, false);
-//        if (view instanceof RecyclerView) {
-//            //Try out a grid layout to achieve rows AND columns. Adjust the widths of the
-//            //cards on display
-//            //((RecyclerView) view).setLayoutManager(new GridLayoutManager(getContext(), 2));
-//            //Try out horizontal scrolling. Adjust the widths of the card so that it is
-//            //obvious that there are more cards in either direction. i.e. don't have the cards
-//            //span the entire witch of the screen. Also, when considering horizontal scroll
-//            //on recycler view, ensure that there is other content to fill the screen.
-//            // ((LinearLayoutManager)((RecyclerView) view).getLayoutManager())
-//            // .setOrientation(LinearLayoutManager.HORIZONTAL);
-//            ((RecyclerView) view).setAdapter(
-//                    new BlogRecyclerViewAdapter(BlogGenerator.getBlogList()));
-//        }
-//        return view;
 
         return inflater.inflate(R.layout.fragment_blog_list,container,false);
     }
