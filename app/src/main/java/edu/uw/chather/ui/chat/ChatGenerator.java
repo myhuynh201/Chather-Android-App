bd6c1575f6299f7759d9fdd7b35e73520b1bbb25
Alec Mac <alecmac95@gmail.com>
2021-05-16 22:18:33 -0700

    reverted file content for chat

--- app/src/main/java/edu/uw/chather/ui/chat/ChatGenerator.java
+++ app/src/main/java/edu/uw/chather/ui/chat/ChatGenerator.java
@@ -2,35 +2,63 @@ package edu.uw.chather.ui.chat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import edu.uw.chather.ui.connections.dummy.ConnectionContent;
 
 /**
- * This class is used to create Dummy Blogs. Use it for development.
- * In future labs, connect to an API to gain actual Blog Data.
+ * This class is used to create Dummy Chats
+ *
+ * @author alecmac
  */
 public final class ChatGenerator {
 
+    /**
+     * Array of chatrooms
+     */
     private static final ChatRoom[] CHAT_ROOMS;
-    public static final int COUNT = 5;
+    /**
+     * Hard coded number of chatrooms
+     */
+    public static final int COUNT = 6;
 
 
     static {
+        Random random = new Random();
         CHAT_ROOMS = new ChatRoom[COUNT];
         for (int i = 0; i < CHAT_ROOMS.length; i++) {
             CHAT_ROOMS[i] = new ChatRoom
-                    .Builder("2020-04-" + (i + 1) + " 12:59 pm",
-                    "Blog Title: Blog Post #" + (i + 43))
-                    .addTeaser("<p>Phish got right down to business last night at Dick&rsquo;s&hellip; so we&rsquo;ll do the same. Roaring out of the gates with &ldquo;Ghost,&rdquo; the band offered only the second show-opening Ghost since the &lsquo;90s, the other also being at Dick&rsquo;s (<a href=\\\"http://phish.net/setlists/?d=2013-08-30&amp;highlight=222\\\">8/31/13</a>, the &ldquo;MOST SHOWS SPELL SOMETHING&rdquo; gig). Rounding out at a little over ten minutes, it was still too early to sense that this was a night where IT was happening.</p><p><img  src=\\\"http://smedia.pnet-static.com/img/herschel_1.png\\\" /><br /><small>Photo by Herschel Gelman.</small></p>")
-                    .addUrl("http://phish.net/blog/1472930164/dicks1-when-mercury-comes-out-at-night")
+                    .Builder("2020-04-" + (12-i) + " 12:59 pm",
+                    ConnectionContent.getName(random.nextInt(17) +1 ))
+                    .addTeaser(makeChat(i))
                     .build();
         }
     }
 
-    public static List<ChatRoom> getChatList() {
-        return Arrays.asList(CHAT_ROOMS);
+    /**
+     * Get dummy chatroom text for preview and viewing
+     * @param position index of the current chatroom being initialized
+     * @return chatroom text
+     */
+    private static String makeChat(int position) {
+        String[] dummyMessages = {
+                "What the heck did you just frickin’ say about me, you little whiner? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills.",
+                "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+                "Excuse me ma’am, not to be disrespectful or rude but could you please take post down. That is my sister who was killed by a metra train. And it this post is very disrespectful. Idk who you are or if you even know her but I need you to take this down please.",
+                "Baby, this is Keke Palmer\r\nHave a good day",
+                "We gon be us\uD83D\uDCAF\uD83D\uDCAF\uD83D\uDC95\uD83D\uDE1D, we gon get crazy \uD83C\uDF1A\uD83D\uDE0E☺\uD83D\uDE48\uD83D\uDE0D✋✋✋✋✋hol' up hol' up ✋✋ looks \uD83D\uDC40 like we got a master \uD83C\uDF93 memer \uD83D\uDC38\uD83D\uDC38\uD83D\uDC38 over here \uD83D\uDC48\uD83D\uDC48",
+                "Hey xQc my son is a huge fan of yours. Anyway over the past few days of watching you play this game hes got much better at playing baseball (he plays in the 11-13 year old division). I just wanted to stop by and thank you for teaching him how to throw so well."
+        };
+        return dummyMessages[position];
     }
 
-    public static ChatRoom[] getChatRooms() {
-        return Arrays.copyOf(CHAT_ROOMS, CHAT_ROOMS.length);
+
+    /**
+     * Get List of dummy chatrooms
+     * @return List of chatrooms
+     */
+    public static List<ChatRoom> getChatList() {
+        return Arrays.asList(CHAT_ROOMS);
     }
 
     private ChatGenerator() { }
--- app/src/main/java/edu/uw/chather/ui/chat/ChatListFragment.java
+++ app/src/main/java/edu/uw/chather/ui/chat/ChatListFragment.java
@@ -2,10 +2,10 @@ package edu.uw.chather.ui.chat;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,54 +14,72 @@ import android.view.ViewGroup;
 import edu.uw.chather.R;
 import edu.uw.chather.databinding.FragmentChatListBinding;
 
-
+/**
+ * This class is used to represent the list of chatrooms
+ *
+ * @author alecmac
+ */
 public class ChatListFragment extends Fragment {
-
+    /**
+     * View model for chat list
+     */
     private ChatListViewModel mModel;
 
+    /**
+     * When ChatListFragment is created, get the view model
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mModel = new ViewModelProvider(getActivity()).get(ChatListViewModel.class);
-        mModel.connectGet();
+//        mModel.connectGet();
 
     }
 
+
+    /**
+     * When view is created, create chats
+     * @param inflater inflates this view
+     * @param container holds this view
+     * @param savedInstanceState represents the saved state
+     * @return the view after being populated
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
-//        if (view instanceof RecyclerView) {
-//            //Try out a grid layout to achieve rows AND columns. Adjust the widths of the
-//            //cards on display
-//            //((RecyclerView) view).setLayoutManager(new GridLayoutManager(getContext(), 2));
-//            //Try out horizontal scrolling. Adjust the widths of the card so that it is
-//            //obvious that there are more cards in either direction. i.e. don't have the cards
-//            //span the entire witch of the screen. Also, when considering horizontal scroll
-//            //on recycler view, ensure that there is other content to fill the screen.
-//            // ((LinearLayoutManager)((RecyclerView) view).getLayoutManager())
-//            // .setOrientation(LinearLayoutManager.HORIZONTAL);
-//            ((RecyclerView) view).setAdapter(
-//                    new ChatRecyclerViewAdapter(ChatGenerator.getChatList()));
-//        }
-//        return view;
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chat_list, container, false);
+        if (view instanceof RecyclerView) {
+            //Try out a grid layout to achieve rows AND columns. Adjust the widths of the
+            //cards on display
+            //((RecyclerView) view).setLayoutManager(new GridLayoutManager(getContext(), 2));
+            //Try out horizontal scrolling. Adjust the widths of the card so that it is
+            //obvious that there are more cards in either direction. i.e. don't have the cards
+            //span the entire witch of the screen. Also, when considering horizontal scroll
+            //on recycler view, ensure that there is other content to fill the screen.
+            // ((LinearLayoutManager)((RecyclerView) view).getLayoutManager())
+            // .setOrientation(LinearLayoutManager.HORIZONTAL);
+            ((RecyclerView) view).setAdapter(
+                    new ChatRecyclerViewAdapter(ChatGenerator.getChatList()));
+        }
+        return view;
 
-        return inflater.inflate(R.layout.fragment_chat_list,container,false);
+//        return inflater.inflate(R.layout.fragment_chat_list,container,false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        super.onViewCreated(view, savedInstanceState);
-        FragmentChatListBinding binding = FragmentChatListBinding.bind(getView());
-
-        mModel.addBlogListObserver(getViewLifecycleOwner(), chatRoomList -> {
-            if (!chatRoomList.isEmpty()) {
-                binding.listRoot.setAdapter( new ChatRecyclerViewAdapter(chatRoomList)
-                );
-                binding.layoutWait.setVisibility(View.GONE);
-            }
-        });
-    }
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        super.onViewCreated(view, savedInstanceState);
+//        FragmentChatListBinding binding = FragmentChatListBinding.bind(getView());
+//
+//        mModel.addChatListObserver(getViewLifecycleOwner(), chatRoomList -> {
+//            if (!chatRoomList.isEmpty()) {
+//                binding.listRoot.setAdapter( new ChatRecyclerViewAdapter(chatRoomList)
+//                );
+//                binding.layoutWait.setVisibility(View.GONE);
+//            }
+//        });
+//    }
 }
