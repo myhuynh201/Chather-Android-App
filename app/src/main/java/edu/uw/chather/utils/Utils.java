62799bd47e573851895f6b60a3f794d71fe808c9
myhuynh201 <77417436+myhuynh201@users.noreply.github.com>
2021-05-19 15:30:19 -0700

    added change theme feature

---
+++ app/src/main/java/edu/uw/chather/utils/Utils.java
@@ -0,0 +1,52 @@
+package edu.uw.chather.utils;
+
+import android.app.Activity;
+
+import edu.uw.chather.MainActivity;
+import edu.uw.chather.R;
+
+public class Utils {
+    private static int sTheme;
+    public final static int BLUE_THEME = 0;
+    public final static int GREEN_THEME  = 1;
+    public final static int PINK_THEME  = 2;
+    public final static int PURPLE_THEME  = 3;
+    public final static int DARK_THEME  = 4;
+
+    /**
+     * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
+     *
+     * @param activity
+     * @param theme
+     */
+    public static void changeToTheme(Activity activity, int theme) {
+        sTheme = theme;
+        activity.recreate();
+    }
+
+    /**
+     * Set the theme
+     *
+     * @param activity
+     */
+    public static void onActivityCreateSetTheme(MainActivity activity) {
+        switch (sTheme) {
+            default:
+            case BLUE_THEME:
+                activity.getTheme().applyStyle(R.style.AppTheme, true);
+                break;
+            case GREEN_THEME:
+                activity.getTheme().applyStyle(R.style.GreenTheme, true);
+                break;
+            case PINK_THEME:
+                activity.getTheme().applyStyle(R.style.PinkTheme, true);
+                break;
+            case PURPLE_THEME:
+                activity.getTheme().applyStyle(R.style.PurpleTheme, true);
+                break;
+            case DARK_THEME:
+                activity.getTheme().applyStyle(R.style.DarkTheme, true);
+                break;
+        }
+    }
+}
