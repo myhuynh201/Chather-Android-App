523e8903b67b72a56b3da3db913df2524609e418
myhuynh201 <77417436+myhuynh201@users.noreply.github.com>
2021-05-19 15:26:40 -0700

    fixed ChangePasswordFragment and ChangePasswordViewModel

--- app/src/main/java/edu/uw/chather/MainActivity.java
+++ app/src/main/java/edu/uw/chather/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uw.chather;
 /**
  * Main activity that runs in the background of the app. Sets up our bottom navigation.
  */
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -11,6 +12,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,20 +24,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.uw.chather.ui.model.UserInfoViewModel;
 import edu.uw.chather.ui.passwordreset.PasswordResetFragment;
+import edu.uw.chather.utils.Utils;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
-
-    /**Creates our bottom navigation menu from the menu elements we've given it before.
+    private NavController navController;
+    /**
+     * Creates our bottom navigation menu from the menu elements we've given it before.
      * See onCreate parent method for more implementation details.
+     *
      * @param savedInstanceState
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
 
         MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
@@ -46,34 +54,56 @@ public class MainActivity extends AppCompatActivity {
         // Check to see if the web token is still valid or not. To make a JWT expire after a
         // longer or shorter time period, change the expiration time when the JWT is
         // created on the web service.
-       // if(!jwt.isExpired(0)) {
+        // if(!jwt.isExpired(0)) {
         //    new ViewModelProvider(this, new UserInfoViewModel.UserInfoViewModelFactory(jwt)).get(UserInfoViewModel.class);
         //} else {
-            //In production code, add in your own error handling/flow for when the JWT is expired
+        //In production code, add in your own error handling/flow for when the JWT is expired
         //    throw new IllegalStateException("JWT is expired!");
         //}
 
         new ViewModelProvider(this,
                 new UserInfoViewModel.UserInfoViewModelFactory(args.getEmail(), args.getJwt())
-                    ).get(UserInfoViewModel.class);
+        ).get(UserInfoViewModel.class);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.success, R.id.navigation_chats, R.id.navigation_connections,  R.id.weatherFragment)
+                R.id.success, R.id.navigation_chats, R.id.navigation_connections, R.id.weatherFragment)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drop_down, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.navigate_change_password) {
+            navController.navigate(R.id.changePasswordFragment);
+        }
+
+        if (id == R.id.navigate_button_theme) {
+            navController.navigate(R.id.changeThemeFragment);
+        }
+
+        if (id == R.id.navigate_sign_out) {
+            //TODO SIGN OUT
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/edu/uw/chather/ui/changepassword/ChangePasswordFragment.java
+++ app/src/main/java/edu/uw/chather/ui/changepassword/ChangePasswordFragment.java
@@ -1,86 +1,178 @@
-//package edu.uw.chather.ui.changepassword;
-//
-//import android.os.Bundle;
-//
-//import androidx.fragment.app.Fragment;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.Button;
-//import android.widget.EditText;
-//
-//import edu.uw.chather.R;
-//import edu.uw.chather.databinding.FragmentChangePasswordBinding;
-//import edu.uw.chather.databinding.FragmentPasswordResetBinding;
-//import edu.uw.chather.utils.PasswordValidator;
-//
-//import static edu.uw.chather.utils.PasswordValidator.checkClientPredicate;
-//import static edu.uw.chather.utils.PasswordValidator.checkExcludeWhiteSpace;
-//import static edu.uw.chather.utils.PasswordValidator.checkPwdDigit;
-//import static edu.uw.chather.utils.PasswordValidator.checkPwdLength;
-//import static edu.uw.chather.utils.PasswordValidator.checkPwdLowerCase;
-//import static edu.uw.chather.utils.PasswordValidator.checkPwdSpecialChar;
-//import static edu.uw.chather.utils.PasswordValidator.checkPwdUpperCase;
-//
-//
-//public class ChangePasswordFragment extends Fragment {
-//
-//    private FragmentChangePasswordBinding binding;
-//
-//    /*
-//        Password validator for the password field in register.
-//     */
-//    private PasswordValidator mPassWordValidator =
-//            checkClientPredicate(pwd -> pwd.equals(binding..getText().toString()))
-//                    .and(checkPwdLength(7))
-//                    .and(checkPwdSpecialChar())
-//                    .and(checkExcludeWhiteSpace())
-//                    .and(checkPwdDigit())
-//                    .and(checkPwdLowerCase().or(checkPwdUpperCase()));
-//
-//    /*
-//        Password validator for the email field in register.
-//    */
-//    private PasswordValidator mEmailValidator = checkPwdLength(2)
-//            .and(checkExcludeWhiteSpace())
-//            .and(checkPwdSpecialChar("@"));
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_change_password, container, false);
-//    }
-//
-//
-//    /**
-//     * Private helper method to check if the password matches the retyped password.
-//     */
-//    private void validatePasswordsMatch() {
-//        PasswordValidator matchValidator =
-//                checkClientPredicate(
-//                        pwd -> pwd.equals(binding.etNewPasswordConfirm.getText().toString().trim()));
-//
-//        mEmailValidator.processResult(
-//                matchValidator.apply(binding.etNewPassword.getText().toString().trim()),
-//                this::validatePassword,
-//                result -> binding.etNewPassword.setError("Passwords must match."));
-//    }
-//
-//    /**
-//     * Private helper method for password validation.
-//     */
-//    private void validatePassword() {
-//        mPassWordValidator.processResult(
-//                mPassWordValidator.apply(binding.etOldPassword.getText().toString()),
-//                this::verifyAuthWithServer,
-//                result -> binding.etOldPassword.setError("Please enter your current password."));
-//    }
-//
-//}
+package edu.uw.chather.ui.changepassword;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uw.chather.databinding.FragmentChangePasswordBinding;
+import edu.uw.chather.ui.model.UserInfoViewModel;
+import edu.uw.chather.utils.PasswordValidator;
+
+import static edu.uw.chather.utils.PasswordValidator.checkClientPredicate;
+import static edu.uw.chather.utils.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.chather.utils.PasswordValidator.checkPwdDigit;
+import static edu.uw.chather.utils.PasswordValidator.checkPwdLength;
+import static edu.uw.chather.utils.PasswordValidator.checkPwdLowerCase;
+import static edu.uw.chather.utils.PasswordValidator.checkPwdSpecialChar;
+import static edu.uw.chather.utils.PasswordValidator.checkPwdUpperCase;
+
+
+public class ChangePasswordFragment extends Fragment {
+
+    private FragmentChangePasswordBinding binding;
+
+
+    private ChangePasswordViewModel mChangePasswordViewModel;
+
+    /**
+     * Method to validate the old password.
+     */
+    private PasswordValidator mOldPasswordValidator = checkPwdLength(7)
+            .and(checkPwdSpecialChar())
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdDigit())
+            .and(checkPwdLowerCase().or(checkPwdUpperCase()));
+
+    /**
+     * Method to validate the new password.
+     */
+    private PasswordValidator mNewPassWordValidator =
+            checkClientPredicate(pwd -> pwd.equals(binding.etNewPass.getText().toString()))
+                    .and(checkPwdLength(7))
+                    .and(checkPwdSpecialChar())
+                    .and(checkExcludeWhiteSpace())
+                    .and(checkPwdDigit())
+                    .and(checkPwdLowerCase().or(checkPwdUpperCase()));
+
+    private String mEmail;
+
+    /**
+     * An empty constructor.
+     */
+    public ChangePasswordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mChangePasswordViewModel = new ViewModelProvider(getActivity())
+                .get(ChangePasswordViewModel.class);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonPasswordReset.setOnClickListener(this::attemptChangePassword);
+        mChangePasswordViewModel.addResponseObserver(getViewLifecycleOwner(),
+                this::observeResponse);
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentChangePasswordBinding.inflate(inflater, container,false);
+        UserInfoViewModel model = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
+        mEmail = model.getEmail();
+        Log.d("EMAIL", mEmail.toString());
+        return binding.getRoot();
+    }
+
+    /**
+     * Private helper method for validation in register.
+     * @param button
+     */
+    private void attemptChangePassword(final View button) {
+        //Toast.makeText(getContext(), "Password Changed Successfully", Toast.LENGTH_LONG);
+        validateOldPassword();
+    }
+
+
+    private void validateOldPassword() {
+        mOldPasswordValidator.processResult(
+                mOldPasswordValidator.apply(binding.etCurrentPass.getText().toString()),
+                this::validateNewPassword,
+                result -> binding.etCurrentPass.setError("Please enter your current password."));
+    }
+
+    /**
+     * Private helper method for password validation.
+     */
+    private void validateNewPassword() {
+        mNewPassWordValidator.processResult(
+                mNewPassWordValidator.apply(binding.etNewPass.getText().toString()),
+                this::verifyAuthWithServer,
+                result -> binding.etCurrentPass.setError("Please enter a valid password."));
+    }
+
+    /**
+     * Private helper method for verification of the register with the server.
+     */
+    private void verifyAuthWithServer() {
+
+        mChangePasswordViewModel.connect(
+                mEmail,
+                binding.etCurrentPass.getText().toString(),
+                binding.etNewPass.getText().toString(),
+                binding.etConfirmNewPass.getText().toString());
+        //This is an Asynchronous call. No statements after should rely on the
+        //result of connect().
+    }
+
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                System.out.print("error");
+                Log.e("Error", "Email Authentication Error");
+//                    mUserViewModel.getEmail().Email.setError(
+//                            "Error Authenticating: " +
+//                                    response.getJSONObject("data").getString("message"));
+                try {
+                    binding.etNewPass.setError("Error Authenticating: " + response.getJSONObject("data").getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                navigateToHome();
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+    }
+
+    private void navigateToHome() {
+        Navigation.findNavController(getView()).navigate(ChangePasswordFragmentDirections.actionChangePasswordFragmentToSuccess());
+
+    }
+
+
+
+
+}
