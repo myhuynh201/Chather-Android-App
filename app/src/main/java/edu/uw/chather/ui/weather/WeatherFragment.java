fa9237ecdd97c1ea97045de48835a1523922dbd2
Alex CO <alejandrocossio1@gmail.com>
2021-05-19 22:38:23 -0700

    Updated the weather fragment and the adapters.

--- app/src/main/java/edu/uw/chather/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/chather/ui/weather/WeatherFragment.java
@@ -1,18 +1,34 @@
 package edu.uw.chather.ui.weather;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 import edu.uw.chather.databinding.FragmentWeatherBinding;
 
 
@@ -24,10 +40,48 @@ public class WeatherFragment extends Fragment {
     //Holds a reference to the WeatherViewModel.
     private WeatherViewModel mViewModel;
 
+
+
+    //Current weather pic
+    //ImageView mImgWeather;
+
+    private TextView txt_city_name,txt_humidity,txt_sunrise,txt_sunset,txt_pressure,txt_temperature,
+            txt_description,txt_date_time,txt_wind,txt_geo_coord;
+
+    private RecyclerView recycler_forecast;
+    private RecyclerView recycler_hourly_forecast;
+    private LinearLayout weather_panel;
+    private ProgressBar loading;
+
+    static WeatherFragment instance;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentWeatherBinding.inflate(inflater, container, false);
+
+        //mImgWeather = (ImageView) binding.imgWeather;
+        txt_city_name = (TextView) binding.txtCityName;
+        txt_humidity = (TextView) binding.txtHumidity;
+        txt_sunrise = (TextView) binding.txtSunrise;
+        txt_sunset = (TextView) binding.txtSunset;
+        txt_pressure = (TextView) binding.txtPressure;
+        txt_temperature = (TextView) binding.txtTemperature;
+        txt_description = (TextView) binding.txtDescription;
+        txt_date_time = (TextView) binding.txtDateTime;
+        txt_wind = (TextView) binding.txtWind;
+        txt_geo_coord = (TextView) binding.txtGeoCoord;
+        recycler_forecast = (RecyclerView) binding.recyclerForecast;
+        recycler_hourly_forecast = (RecyclerView) binding.recyclerHourlyForecast;
+        recycler_hourly_forecast.setHasFixedSize(true);
+        recycler_hourly_forecast.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false));
+        recycler_forecast.setHasFixedSize(true);
+        recycler_forecast.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false));
+
+        weather_panel = (LinearLayout) binding.weatherPanel;
+        loading = (ProgressBar) binding.loading;
         return binding.getRoot();
     }
 
@@ -39,8 +93,16 @@ public class WeatherFragment extends Fragment {
                 .get(WeatherViewModel.class);
     }
 
+    public WeatherFragment() {
 
+    }
 
+    public static WeatherFragment getInstance() {
+        if(instance == null){
+            instance = new WeatherFragment();
+        }
+        return instance;
+    }
     //        mViewModel.connect(String.format("%.2f",location.getLatitude()),
     //                String.format("%.2f",location.getLongitude()));
 
@@ -56,19 +118,40 @@ public class WeatherFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         //Send the Chicago hard-coded location coordinates
-        mViewModel.connect("-94.04","33.44");
+        mViewModel.connect();
         //This should provide an observer to the view model, to check responses
         mViewModel.addResponseObserver(getViewLifecycleOwner(),
                 this::observeResponse);
-
     }
 
-    //I need to add a function here that will do everything that I need to once I have
-    //received all the data that I have requested from the web service
+
     public void displayWeather(JSONObject response) {
-        //Here i will do something to display the weather from web service.
+
+        //Loading the information
         try{
-            Log.d("Location", response.getString("lat"));
+            txt_city_name.setText("Tacoma, WA");
+            txt_description.setText(new StringBuilder("Weather in Tacoma, WA"));
+            txt_temperature.setText(response.getJSONObject("current").getString("temp") + "°F");
+            Date date = new Date(Integer.parseInt(response.getJSONObject("current").getString("dt")) * 1000L);
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mma EEE MM/d/yyyy");
+            txt_date_time.setText(sdf.format(date));
+            txt_pressure.setText(response.getJSONObject("current").getString("pressure") + " hPa");
+            txt_humidity.setText(response.getJSONObject("current").getString("humidity") + " %");
+            date = new Date(Integer.parseInt(response.getJSONObject("current").getString("sunrise")) * 1000L);
+            sdf = new SimpleDateFormat("hh:mma ");
+            txt_sunrise.setText(sdf.format(date));
+            date = new Date(Integer.parseInt(response.getJSONObject("current").getString("sunset")) * 1000L);
+            sdf = new SimpleDateFormat("hh:mma ");
+            txt_sunset.setText(sdf.format(date));
+            txt_geo_coord.setText("[" + response.getString("lat") + "," +
+                    response.getString("lon") + "]");
+            WeatherForecastAdapter adapter = new WeatherForecastAdapter(getContext(), response);
+            WeatherHourlyForecastAdapter madapter = new WeatherHourlyForecastAdapter(getContext(), response);
+            recycler_hourly_forecast.setAdapter(madapter);
+            recycler_forecast.setAdapter(adapter);
+            //Displaying the panel
+            weather_panel.setVisibility(View.VISIBLE);
+            loading.setVisibility(View.GONE);
         } catch (JSONException e) {
             e.printStackTrace();
         }
